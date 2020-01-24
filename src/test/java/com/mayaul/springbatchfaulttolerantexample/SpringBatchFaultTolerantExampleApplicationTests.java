0e044f69a078f2ebaeb048c85cdf9262c25cdedb
mayaul <rookey03@naver.om>
2020-01-24 17:37:19 +0900

    20200124

---
+++ src/test/java/com/mayaul/springbatchfaulttolerantexample/SpringBatchFaultTolerantExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayaul.springbatchfaulttolerantexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchFaultTolerantExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
